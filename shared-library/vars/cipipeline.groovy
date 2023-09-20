9881a3430e4a2c94dae7a116ae0ddca2ea5dfcb0
sravan k <sravankakireddy@gmail.com>
2023-09-20 08:44:53 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -36,10 +36,13 @@ def call() {
             }
 
             stage('Code Security') {
-//              when {
-//                    expression {BRANCH_NAME=="main"}
-//                    expression {BRANCH_NAME=="test1"}
-//                }
+              when {
+                  allof{
+                      expression {BRANCH_NAME=="main"}
+                      expression {BRANCH_NAME=="test1"}
+                  }
+
+                }
                 steps {
                     echo 'Hello World'
                     sh 'env'
