a91a8bb2c65087a85423ae6664b4755d1db9d19f
sravan k <sravankakireddy@gmail.com>
2023-09-21 20:11:52 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,6 +1,16 @@
 def call() {
     node ('workstation') {
-        git branch: "${BRANCH_NAME}", url: "https//github.com/sravanreddy28/${component}"
+        sh "find . | sed -e '1d' |xargs rm -rf"
+        git branch: 'main', url: "https://github.com/sravanreddy28/${component}"
+////        if (env.TAG_NAME ==~ "'*") {
+//            env.branch_name = "refs/tags/${env.TAG_NAME}"
+//        } else {
+//            env.branch_name = "${env.BRANCH_NAME}"
+//        }
+//        checkout scmGit(
+//                branches: [[name: branch_name]],
+//                userRemoteConfigs: [[url: "https://github.com/sravanreddy28/${component}"]]
+//        )
         stage ('compile code') {
             common.compile()
         }
--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,14 +1,14 @@
 def compile() {
-    if (env.codeType=="maven") {
+    if (env.codeType == "maven") {
         print 'maven'
     }
-    if (env.codeType=="nodejs") {
+    if (env.codeType == "nodejs") {
         print 'nodejs'
     }
-    if (env.codeType=="python") {
+    if (env.codeType == "python") {
         print 'python'
     }
-    if (env.codeType=="static") {
+    if (env.codeType == "static") {
         print 'static'
     }
 }
