9e3a44ca20703d7276f7aa3f7ee1209b1263752b
sravan k <sravankakireddy@gmail.com>
2023-09-22 14:23:51 +0530

    jenkins

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -35,8 +35,7 @@ def codequality() {
         env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
 
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
-
-//            sh 'sonar-scanner -Dsonar.host.url=http://172.31.41.185:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+            sh 'sonar-scanner -Dsonar.host.url=http://172.31.41.185:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
         }
     }
 }
